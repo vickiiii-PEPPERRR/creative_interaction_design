9537a7d7ba3867d5bfd4da6145ae83070a908ca9
vickiiii-PEPPERRR <100364166+vickiiii-PEPPERRR@users.noreply.github.com>
2022-04-15 09:32:38 +0800

    添加

---
+++ button.js
@@ -0,0 +1,12 @@
+function alipay(){
+  document.getElementById("Alipay").innerHTML="yes!"
+}
+function eleme(){
+  document.getElementById("Eleme").innerHTML="Eleme bule"
+}
+function tiffany(){
+  document.getElementById("tiffany").innerHTML="tiffany bule"
+}
+function KleinBlue(){
+  document.getElementById("KleinBlue").innerHTML="KleinBlue"
+}
